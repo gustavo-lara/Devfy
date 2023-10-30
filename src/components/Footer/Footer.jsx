73250a1a17a402194cff835a5fdb1b6588b93992
Gustavo Lara <126248749+gustavo-lara@users.noreply.github.com>
2023-10-30 23:43:36 +0000

    Colocando README mudando nome no index comecando o projeto criando o Login

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import styles from  "./Footer.module.css"
-import insta from "../../assets/logo-instagram.svg"
-import tiktok from "../../assets/logo-tiktok.svg"
-import github from "../../assets/logo-github.svg"
+import styles from "./Footer.module.css"
+
 
 
 const Footer = () => {
   return (
     <div className={styles.div}>
-     <a href="https://www.instagram.com/_gustavolara_/"><img src={insta} alt="" /></a>
-     <a href="https://www.tiktok.com/@gustavolara_"><img src={tiktok} alt="" /></a>
-     <a href="https://github.com/gustavo-lara"><img src={github} alt="" /></a>
+
     </div>
   )
 }
