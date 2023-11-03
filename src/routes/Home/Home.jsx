1c481778ea39c9fe8ffe6605333b3f5206042cb7
gustavo-lara <gustavolara63719@gmail.com>
2023-11-03 15:56:09 -0300

    Teste git

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -3,7 +3,7 @@ import styles from "./Home.module.css"
 export const Home = () => {
   return (
     <div className={styles.div}>
-      Home
+      Teste
       {/* Nav(Buscar, inicio) (Componente, mostrar as routas na div da direita)
       O Home e a div da direita, se clicar Home, aparece esssa tela, se clicar Buscar, 
       aparece outra rota que é a Buscar*/}
