4b366828ee67ff6bf34e39b84eb6f007ed36785f
gustavo-lara <gustavolara63719@gmail.com>
2023-11-14 15:47:20 -0300

    Excluindo o btn de play por conta de bugs.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,6 @@ const Card = ({img, p, h4}) => {
   return (
     <div className={styles.card}>
         <img src={img} alt="" />
-        <div className={styles.play}><ion-icon name="play"></ion-icon></div>
         <div className={styles.cont}>
         <p>{p}</p>
         <h4>{h4}</h4>
